e292b601c6fedb50a16bf9e8a0f3d907c4ec5d89
Insydian <cr.larue03@gmail.com>
2023-02-18 21:36:58 -0600

    implement a fix for the delete data function.
    The delete data function was attempting to print the whole object to the
    console but that is not allowed when constructing Objects. I first tried
    to overload the operator'<<' as a member function but the compiler did
    not like this. ChatGPT suggested I try overloading it as a global
    function. It worked but I am not totally sure why. I think it has
    something to do with needing to use all member variables maybe. But once
    you move it to a global function that need goes away.

--- Song.cpp
+++ Song.cpp
@@ -4,13 +4,14 @@
 #include <thread>
 #include <string>
 #include <forward_list>
+#include <sstream>
 #include "LinkedList.h"
 using namespace std;
 struct Song
 {
     string composerName; 
     string songName; 
-    ;
+    
     Song() {};
     Song(string composerName, string songName)
         :composerName(composerName),
@@ -29,7 +30,35 @@ struct Song
 //
 //
 //    }
+    bool operator==(const Song aSong)
+    {
+        if(this->composerName == aSong.composerName && this->songName == aSong.songName)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    bool operator!=(const Song aSong)
+    {
+        if(this->composerName == aSong.composerName && this->songName == aSong.songName)
+        {
+            return false;
+        }
+        else 
+        {
+            return true;
+        }
+
+    }
+
 };
+std::ostream& operator<<( std::ostream& os, const Song& song) {
+    os << song.songName;
+    return os;
+}
 Song randomSongGenerator()
 {
     string firstName[]= {"John", "Ludwig", "Hildegard" , "George", "Antonio", "Claude"};
@@ -38,7 +67,6 @@ Song randomSongGenerator()
     string songAdjectives[] = {"Mystery" , "Fire of my", "Soul", "Depths", "Without", "Remember"};
     string songNoun[] = {"Power", "Friend", "Midnight", "Yesterday", "Story", "Blues"};
 
-    srand(0);
     // make the song 
     string fullName = firstName[ std::rand()/((RAND_MAX + 1u)/6)] + " " + lastName[std::rand()/((RAND_MAX + 1u)/6)];
     string songName = songAdjectives[std::rand()/((RAND_MAX + 1u)/6)] + " " + songNoun[std::rand()/((RAND_MAX + 1u)/6)]; 
@@ -51,17 +79,8 @@ Song randomSongGenerator()
 
 int main ()
 {
-    std::forward_list<Song> songList;
-    for(int i = 0; i < 20; ++i)
-    {
-        songList.push_front(randomSongGenerator());
-    }
-    for (std::forward_list<Song>::iterator it = songList.begin(); it != songList.end(); ++it)
-    {
-        std::cout << it -> songName << endl;
-    }
     LinkedList<Song> SongList;
-    for(int i = 0; i < 20; ++i)
+    for(int i = 0; i < 100; ++i)
     {
         Song aSong = randomSongGenerator();
         SongList.insertFirst(aSong);
@@ -73,7 +92,29 @@ int main ()
     cout << "-------------------------------------------------------------" << endl;
     for(int i = 0; i < SongList.length(); ++i)
     {
-        std::cout << SongList.getNode(i)->info.songName << std::endl;
+        //std::cout << SongList.getNode(i)->info.songName << std::endl;
     }
+    Song Radioactive("Imagine Dragons", "Radioactive");
+    Song N95("Kendrick Lamar" , "N95"); 
+    SongList.insertAfter(SongList.getNode(SongList.length()-1), Radioactive);
+    SongList.insertAfter(SongList.getNode(SongList.length()/2), N95); 
+    if(SongList.search(Radioactive))
+    {
+        cout << "found " << Radioactive.songName << endl;
+    }
+    else 
+    {
+        cout << "didn't find it" << endl;
+    }
+    if(SongList.search(N95))
+    {
+        cout << "found " << N95.songName << endl;
+    }
+    else 
+    {
+        cout << "didn't find it!" << endl;
+    }
+    SongList.deleteNodeWithInfo(N95);
+    cout << "deleted the song N95" << endl;
     return 0;
 }
